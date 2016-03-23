3fd4ef9bb2815ba5624b4e2d1335e968c2a79c5f
toastynerd <tylermorgan86@gmail.com>
2016-03-23 12:35:29 -0700

    change styling

--- app/client.js
+++ app/client.js
@@ -2,7 +2,7 @@ var socket = window.io();
 var tweetList = document.getElementById('tweetlist');
 var prependTweet = (tweet) => {
     var tweetEl = document.createElement('li');
-    tweetEl.innerHTML = '<img src="' + tweet.user.profile_image_url + '" alt="profile image">' + tweet.text; 
+    tweetEl.innerHTML = '<img src="' + tweet.user.profile_image_url + '" alt="profile image"><p>' + tweet.text + '</p>'; 
     tweetList.insertBefore(tweetEl, tweetList.firstChild);
 };
 
